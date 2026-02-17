b9a6da515993d7ff724794f1228f483929ad6e8e
GroupP04 <makaiojimenez@berkeley.edu>
2026-02-17 14:24:20 -0800

    good progress regarding out noise gaussian fit

--- SDR_gather.py
+++ SDR_gather.py
@@ -7,26 +7,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Title of Data Trial
-trial = "freq1420.405" 
+trial = "Zenith_test3" 
 sample_rate = 2.9e6
 block_size = 131072 
-blocks = 4 
+blocks = 10
 
 # Data Collection Information
-frequency = 1420.405
-frequency_units = "MHz"
+frequency = "sky"
+frequency_units = "Hz"
 amplitude = 0
 amplitude_units = "dBm" #readout from wave generator. stored in data as ADC
 direct = False
 unix_time = ugradio.timing.unix_time()
 local_time = ugradio.timing.local_time()
+gain = 3
 location = "NCH" 
-direction = "8th rung from the left, pointing north (aligned with square at bottom), above red roof"
-notes = "Forgot to change the name, the previous file names freq1420 is also 1420.405 MHz. Moving frequency to confirm we are observing the correct signal."
+direction = "Zenith"
+notes = "Trying to find 21 cm with histogram with horn pointed at zenith.First"
 
 # Note: Use this text file to collect aliased data 
 sdr = ugradio.sdr.SDR(direct=False, center_freq=1420.395e6, sample_rate=2.9e6, gain=3, fir_coeffs=None)
-data = sdr.capture_data(131072, nblocks=4)
+data = sdr.capture_data(131072, nblocks=10)
 print(sdr)
 print(data)
 
@@ -58,7 +59,7 @@ plt.title("Power")
 print(plt.show())
 
 # Saving info as numpy array
-info = np.array([f"File:{trial}",f"Frequency:{frequency} {frequency_units}", f"Amplitude:{amplitude} {amplitude_units}", f"Sampling Rate:{sample_rate}", f"Direct:{direct}", f"Unix: {unix_time}", f"Local: {local_time}", f"Location: {location}", f"Direction: {direction}", f"Notes: {notes}"])
+info = np.array([f"File:{trial}",f"Frequency:{frequency} {frequency_units}", f"Amplitude:{amplitude} {amplitude_units}", f"Sampling Rate:{sample_rate}", f"Direct:{direct}", f"Unix: {unix_time}", f"Local: {local_time}",f"Gain: {gain}", f"Location: {location}", f"Direction: {direction}", f"Notes: {notes}"])
 print(info)
 
 # Saving data into .npz zip file
